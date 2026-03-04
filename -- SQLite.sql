00e2009773e0a6c13d9fef6b4376b200b0d44e4b
Simon <23reads@mountainview.school.nz>
2026-03-05 09:54:56 +1300

    Changed insert

--- -- SQLite.sql
+++ -- SQLite.sql
@@ -5,6 +5,19 @@
 SELECT * from Student ORDER BY date_of_birth;
 SELECT * from Student WHERE first_name = 'Gene';
 
-UPDATE Himi
-SET building_name = 'Himi'  
-WHERE building_name = 'Hiwi';                                                          
+INSERT INTO Student (first_name, last_name, date_of_birth)
+VALUES ("Joshua", "Jakob", "2000-05-12");      
+SELECT * from Student;    
+
+INSERT INTO Teacher (teacher_name)
+VALUES ("Mr Paul");
+SELECT * from Teacher;
+
+INSERT INTO Class (class_name, level, teacher_id, room_id)
+VALUES
+("English", "2", "4", "24"); 
+SELECT * FROM  Class;
+
+UPDATE Student 
+SET '4'
+WHERE condition; 
